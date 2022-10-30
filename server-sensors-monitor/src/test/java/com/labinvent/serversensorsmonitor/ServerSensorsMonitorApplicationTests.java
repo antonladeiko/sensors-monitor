9977b96fb68bf6bee827a57f1ac0be3270a4ec0f
Ladeiko752 <ladeikolkd@mail.ru>
2022-10-30 14:28:05 +0300

    initial commit

---
+++ server-sensors-monitor/src/test/java/com/labinvent/serversensorsmonitor/ServerSensorsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labinvent.serversensorsmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSensorsMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
